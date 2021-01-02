40155b4d4da478d1365ad5513b2c697f7451ec2b
obrainx <nebiguler97@gmail.com>
2021-01-02 22:06:06 +0300

    Startup.cs
    form boyutundan dolayı video yüklenemiyordu arttırmak için eklendi ama başka bir yöntem gerekiyor

--- TrickingLibrary.Api/Startup.cs
+++ TrickingLibrary.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,14 +17,19 @@ namespace TrickingLibrary.Api
         private const string AllCors = "All";
         public void ConfigureServices(IServiceCollection services)
         {
-          
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = int.MaxValue;
+                x.MultipartBodyLengthLimit = int.MaxValue; // if don't set default value is: 128 MB
+                x.MultipartHeadersLengthLimit = int.MaxValue;
+            });
             services.AddControllers();
             services.AddSingleton<TrickyStore>();
             services.AddCors(options =>
                 options.AddPolicy(AllCors,build => build.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod()));
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         {
             if (env.IsDevelopment())
             {
