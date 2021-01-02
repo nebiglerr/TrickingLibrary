cbad2f4638ff969827289df86b6c0dd9469aeda4
obrainx <nebiguler97@gmail.com>
2021-01-02 22:01:07 +0300

    VideoControllers
    yüklenen videonun klasör yolundan çekilmesi

--- TrickingLibrary.Api/Controllers/VideosController.cs
+++ TrickingLibrary.Api/Controllers/VideosController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 
 namespace TrickingLibrary.Api.Controllers
 {
@@ -13,9 +14,17 @@ namespace TrickingLibrary.Api.Controllers
     {
         private readonly IWebHostEnvironment _env;
 
-        public VideosController(IWebHostEnvironment env)
+        public VideosController(IWebHostEnvironment env) 
         {
             _env = env;
+        }
+
+        [HttpGet("{video}")]
+        public async Task<IActionResult> GetVideo(string video)
+        {
+            var savePath = Path.Combine(_env.WebRootPath, video);
+          
+            return new FileStreamResult( new FileStream(savePath, FileMode.Open, FileAccess.Read), "video/*");
         }
         [HttpPost]
         public async Task<IActionResult> UploadVideo(IFormFile video)
