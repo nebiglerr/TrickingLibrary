011e669b7782b34f34dc74ce58563d5e986f822b
obrainx <nebiguler97@gmail.com>
2021-01-02 22:04:21 +0300

    default.vue
    appbar eklenerek upload tuşu eklendi pop içerisinde çıkıyor

--- web-client/layouts/default.vue
+++ web-client/layouts/default.vue
@@ -1,15 +1,30 @@
 <template>
   <v-app dark>
+    <v-app-bar app dark>
+      <v-toolbar-title>Tircking Lirary</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn depressed @click="toggleActivity">
+        <v-icon small>
+          Upload
+        </v-icon>
+      </v-btn>
+    </v-app-bar>
+    <video-upload />
     <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <nuxt />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+import VideoUpload from '../components/video-upload'
 export default {
-
+  methods: mapMutations('video-upload', ['toggleActivity']),
+  component: {
+    VideoUpload
+  }
 }
 </script>
