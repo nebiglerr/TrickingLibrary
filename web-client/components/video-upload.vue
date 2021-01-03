1939fd5801a27a217895568e22dcee547da3cc3e
obrainx <nebiguler97@gmail.com>
2021-01-03 17:34:13 +0300

    video-upload.vue
    Upload taşınarak slot olarak eklendi enum üzerinde olusturulan trick yada submisions controlü sağlandı  ardından stepler eklenere kontrol sağlandı

--- web-client/components/video-upload.vue
+++ web-client/components/video-upload.vue
@@ -1,5 +1,10 @@
 <template>
   <v-dialog :value="active" persistent width="500">
+    <template v-slot:activator="{on}">
+      <v-btn depressed v-on="on" @click="toggleActivity">
+        Upload
+      </v-btn>
+    </template>
     <v-card>
       <v-stepper v-model="step">
         <v-stepper-header>
@@ -7,19 +12,24 @@
             Select Type
           </v-stepper-step>
 
-          <v-divider />
+          <v-divider v-if="type === uploadType.TRICK" />
 
-          <v-stepper-step :complete="step > 2" step="2">
-            Upload Video
-          </v-stepper-step>
-          <v-divider />
-
-          <v-stepper-step :complete="step > 3" step="3">
+          <v-stepper-step v-if="type === uploadType.TRICK" :complete="step > 2" step="2">
             Trick Information
           </v-stepper-step>
           <v-divider />
 
-          <v-stepper-step step="4">
+          <v-stepper-step :complete="step > 3" step="3">
+            Upload Video
+          </v-stepper-step>
+          <v-divider />
+
+          <v-stepper-step :complete="step > 4" step="4">
+            Submission Information
+          </v-stepper-step>
+          <v-divider />
+
+          <v-stepper-step step="5">
             Review
           </v-stepper-step>
         </v-stepper-header>
@@ -27,40 +37,43 @@
         <v-stepper-items>
           <v-stepper-content step="1">
             <div class="d-flex flex-column align-center">
-              <v-btn class="my-2" @click="setType(uploadType.TRICK)">
+              <v-btn class="my-2" @click="setType({type:uploadType.TRICK})">
                 Tricks
               </v-btn>
-              <v-btn class="my-2" @click="setType(uploadType.SUBMISSION)">
+              <v-btn class="my-2" @click="setType({type:uploadType.SUBMISSION})">
                 Submission
               </v-btn>
             </div>
           </v-stepper-content>
           <v-stepper-content step="2">
-            <v-file-input accept="video/*" @change="handleFile($event)" />
-          </v-stepper-content>
-
-          <v-stepper-content step="3">
             <div>
               <v-text-field v-model="trickName" label="Tricking Name" />
-              <v-btn @click="saveTrick">
+              <v-btn @click="incStep">
                 Save Tricks
               </v-btn>
               <div />
             </div>
           </v-stepper-content>
+          <v-stepper-content step="3">
+            <v-file-input accept="video/*" @change="handleFile($event)" />
+          </v-stepper-content>
 
           <v-stepper-content step="4">
             <div>
-              Success
+              <v-text-field v-model="submission" label="Description" />
+              <v-btn @click="incStep">
+                Save Submission
+              </v-btn>
+              <div />
             </div>
+          </v-stepper-content>
 
-            <v-btn color="primary" @click="step = 1">
-              Continue
-            </v-btn>
-
-            <v-btn text>
-              Cancel
-            </v-btn>
+          <v-stepper-content step="5">
+            <div>
+              <v-btn @click="save">
+                Save
+              </v-btn>
+            </div>
           </v-stepper-content>
         </v-stepper-items>
       </v-stepper>
@@ -81,19 +94,18 @@ export default {
   name: 'VideoUpload',
 
   data: () => ({
-    trickName: ''
+    trickName: '',
+    submission: ''
 
   }),
   computed: {
-    ...mapState('video-upload', ['uploadPromise', 'active', 'step']),
+    ...mapState('video-upload', ['uploadPromise', 'active', 'step', 'type']),
     uploadType () {
-      return {
-        ...UPLOAD_TYPE
-      }
+      return UPLOAD_TYPE
     }
   },
   methods: {
-    ...mapMutations('video-upload', ['resetVideos', 'toggleActivity', 'setType']),
+    ...mapMutations('video-upload', ['resetVideos', 'incStep', 'toggleActivity', 'setType']),
     ...mapActions('video-upload', ['startVideoUpload', 'createTricks']),
 
     async handleFile (files) {
@@ -110,14 +122,22 @@ export default {
       // console.log(formData)
       await this.startVideoUpload({ formData })
     },
-    async saveTrick () {
+    async save () {
       if (!this.uploadPromise) {
         return
       }
       const video = await this.uploadPromise
       console.log(video)
-      await this.createTricks({ tricks: { name: this.trickName, video } })
+      await this.createTricks({
+        tricks: { name: this.trickName },
+        submission: {
+          description: this.submission,
+          trickId: 1,
+          video
+        }
+      })
       this.trickName = ''
+      this.submission = ''
 
       this.resetVideos()
     }
