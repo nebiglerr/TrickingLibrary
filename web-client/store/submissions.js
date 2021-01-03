925203ea8e5113bcc282ac2222e944a20774a651
obrainx <nebiguler97@gmail.com>
2021-01-03 17:32:52 +0300

    submissions.js
    submissions.js created tricks.js copy paste

---
+++ web-client/store/submissions.js
@@ -0,0 +1,29 @@
+﻿export const strict = false
+const initState = () => ({
+  submissions: []
+})
+
+export const state = initState
+
+export const mutations = {
+  setSubmissions (state, submissions) {
+    state.submissions = submissions
+  },
+  resetTricks (state) {
+    Object.assign(state, initState())
+  }
+}
+
+export const actions = {
+  fetchSubmissions ({ commit }) {
+    this.$axios.$get('/api/submissions').then((submissions) => {
+      commit('setSubmissions', submissions)
+    })
+    // console.log('tricks :', tricks)
+  }
+
+  /* async nuxtServerInit({dispatch}) {
+    await dispatch("auth/initialize")
+    await dispatch("library/loadContent")
+  } */
+}
