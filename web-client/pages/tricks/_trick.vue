0d42954d80e6d5cd64ca0363a140d5280873cb0c
obrainx <nebiguler97@gmail.com>
2021-01-04 10:17:37 +0300

    Create _trick.vue
    Kaldırılan submmisions yönlendirme olarak  yeni t_ricks.vue dosyasına yönlendirildi

---
+++ web-client/pages/tricks/_trick.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="d-flex justify-center align-start">
+    <div v-if="submissions" class="mx-2">
+      <div v-for="x in 20" :key="x.id">
+        <div v-for="s in submissions" :key="s.id">
+          {{ s.id }}-{{ s.description }}-{{ s.trickId }}
+          <div>
+            <video width="400" controls :src="'http://localhost:5000/api/videos/'+s.video" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="mx-2 sticky">
+      Tricks : {{ $route.params.trick }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: mapState('submissions', ['submissions']),
+  async fetch () {
+    const trickId = this.$route.params.trick
+    await this.$store.dispatch('submissions/fetchSubmissionsForTricks', trickId, { root: true })
+  }
+}
+</script>
+
+<style scoped>
+.sticky{
+position: -webkit-sticky;
+  position: sticky;
+  top: 62px;
+}
+</style>
