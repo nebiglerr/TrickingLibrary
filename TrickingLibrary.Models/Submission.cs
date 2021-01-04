88efc5c2e0a9f5a8ff2030b353ee03570f196221
obrainx <nebiguler97@gmail.com>
2021-01-04 10:11:46 +0300

    Update Submission.cs
    Submission edit base model

--- TrickingLibrary.Models/Submission.cs
+++ TrickingLibrary.Models/Submission.cs
@@ -1,9 +1,9 @@
 ﻿namespace TrickingLibrary.Models
 {
-    public class Submission : BaseModel
+    public class Submission : BaseModel<int>
     {
      
-        public int TrickId { get; set; }
+        public string TrickId { get; set; }
         public string Video { get; set; }
         public string Description { get; set; }
     }
