a9bae38348688a8013be4e172278485e7110caa2
obrainx <nebiguler97@gmail.com>
2021-01-05 09:38:25 +0300

    content-creation-dialog
    Bölünen video-upload.vue yönlendirme için content-creation-dialog eklendi

---
+++ web-client/components/content-creation/content-creation-dialog.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-dialog :value="active" persistent>
+    <template v-slot:activator="{ on}">
+      <v-menu offset-y v-on="on">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn depressed v-bind="attrs" v-on="on">
+            Create
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, i) in menuItems" :key="'ccd-menu-'+i" @click="activate({component:item.component})">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </template>
+    <div v-if="component">
+      <component :is="component" />
+    </div>
+    <div class="d-flex justify-center my-4">
+      <v-btn @click="resetVideos">
+        Closed
+      </v-btn>
+    </div>
+  </v-dialog>
+</template>
+
+<script>
+import { mapMutations, mapState } from 'vuex'
+import TrickStep from './trick-step'
+import SubmissionsSteps from './submission-step'
+
+export default {
+  name: 'ContentCreationDialog',
+
+  component: {
+    TrickStep,
+    SubmissionsSteps
+  },
+  computed: {
+    ...mapState('video-upload', ['active', 'component']),
+    menuItems () {
+      return [
+        { component: TrickStep, title: ' Trick' },
+        { component: SubmissionsSteps, title: 'Submission ' }
+      ]
+    }
+  },
+  methods: mapMutations('video-upload', ['resetVideos', 'activate'])
+}
+</script>
+
+<style scoped>
+
+</style>
